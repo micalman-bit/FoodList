d156fefd8af30d25758ce9de6117c4d9be72cdd2
Andrey <micalman@list.ru>
2022-10-21 22:30:59 +0300

    Production V0.0.1

---
+++ FoodList/FoodListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodListApp.swift
+//  FoodList
+//
+//  Created by Andrey Samchenko on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct FoodListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
